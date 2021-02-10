efec080bde63f94cac565070ab68533b6ffcb1f7
webhifzur <webhifzur@gmail.com>
2021-02-10 10:52:33 +0600

    add package cart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
         <link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.ico') }}">
 
-        <title>Adminto - Responsive Admin Dashboard Template</title>
+        <title>Abu Sufian</title>
 
         <!--Morris Chart CSS -->
 		<link rel="stylesheet" href="{{ asset('admin/assets/plugins/morris/morris.css') }}">
@@ -130,6 +130,10 @@
                             </li>
 
                             <li>
+                                <a href="{{ route('package') }}" class="waves-effect"><i class="mdi mdi-format-font"></i> <span> Package </span> </a>
+                            </li>
+
+                            <li>
                                 <a href="typography.html" class="waves-effect"><i class="mdi mdi-format-font"></i> <span> Typography </span> </a>
                             </li>
 
@@ -151,84 +155,6 @@
                                     <li><a href="ui-sweetalert.html">Sweet Alert</a>
                                     <li><a href="ui-treeview.html">Tree view</a>
                                     <li><a href="ui-widgets.html">Widgets</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-texture"></i><span class="badge badge-warning pull-right">7</span><span> Forms </span> </a>
-                                <ul class="list-unstyled">
-                                    <li><a href="form-elements.html">General Elements</a></li>
-                                    <li><a href="form-advanced.html">Advanced Form</a></li>
-                                    <li><a href="form-validation.html">Form Validation</a></li>
-                                    <li><a href="form-wizard.html">Form Wizard</a></li>
-                                    <li><a href="form-fileupload.html">Form Uploads</a></li>
-                                    <li><a href="form-wysiwig.html">Wysiwig Editors</a></li>
-                                    <li><a href="form-xeditable.html">X-editable</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-view-list"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                	<li><a href="tables-basic.html">Basic Tables</a></li>
-                                    <li><a href="tables-datatable.html">Data Table</a></li>
-                                    <li><a href="tables-responsive.html">Responsive Table</a></li>
-                                    <li><a href="tables-editable.html">Editable Table</a></li>
-                                    <li><a href="tables-tablesaw.html">Tablesaw Table</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-chart-donut-variant"></i><span> Charts </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="chart-flot.html">Flot Chart</a></li>
-                                    <li><a href="chart-morris.html">Morris Chart</a></li>
-                                    <li><a href="chart-chartist.html">Chartist Charts</a></li>
-                                    <li><a href="chart-chartjs.html">Chartjs Chart</a></li>
-                                    <li><a href="chart-other.html">Other Chart</a></li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                <a href="calendar.html" class="waves-effect"><i class="mdi mdi-calendar"></i><span> Calendar </span></a>
-                            </li>
-
-                            <li>
-                                <a href="inbox.html" class="waves-effect"><i class="mdi mdi-email"></i><span class="badge badge-purple pull-right">New</span><span> Mail </span></a>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-google-pages"></i><span> Pages </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="page-starter.html">Starter Page</a></li>
-                                    <li><a href="page-login.html">Login</a></li>
-                                    <li><a href="page-register.html">Register</a></li>
-                                    <li><a href="page-recoverpw.html">Recover Password</a></li>
-                                    <li><a href="page-lock-screen.html">Lock Screen</a></li>
-                                    <li><a href="page-confirm-mail.html">Confirm Mail</a></li>
-                                    <li><a href="page-404.html">Error 404</a></li>
-                                    <li><a href="page-500.html">Error 500</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-layers"></i><span>Extra Pages </span> <span class="menu-arrow"></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="extras-projects.html">Projects</a></li>
-                                    <li><a href="extras-tour.html">Tour</a></li>
-                                    <li><a href="extras-taskboard.html">Taskboard</a></li>
-                                    <li><a href="extras-taskdetail.html">Task Detail</a></li>
-                                    <li><a href="extras-profile.html">Profile</a></li>
-                                    <li><a href="extras-maps.html">Maps</a></li>
-                                    <li><a href="extras-contact.html">Contact list</a></li>
-                                    <li><a href="extras-pricing.html">Pricing</a></li>
-                                    <li><a href="extras-timeline.html">Timeline</a></li>
-                                    <li><a href="extras-invoice.html">Invoice</a></li>
-                                    <li><a href="extras-faq.html">FAQ</a></li>
-                                    <li><a href="extras-gallery.html">Gallery</a></li>
-                                    <li><a href="extras-email-template.html">Email template</a></li>
-                                    <li><a href="extras-maintenance.html">Maintenance</a></li>
-                                    <li><a href="extras-comingsoon.html">Coming soon</a></li>
                                 </ul>
                             </li>
 
@@ -254,11 +180,7 @@
 
                 <!-- Start content -->
                 <div class="content">
-
-                    
-                    
-                    @yield('section')
-
+                    @yield('content')
                 </div> <!-- content -->
 
                 <footer class="footer text-right">
